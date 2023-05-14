bb6f39de130c9aac427e4159933241822d720d5a
saadDevlop <105066118+saadDevlop@users.noreply.github.com>
2023-05-14 11:31:13 +0100

    1

---
+++ src/main/java/com/pfa/SOmedical/somedical/SomedicalApplication.java
@@ -0,0 +1,13 @@
+package com.pfa.SOmedical.somedical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomedicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomedicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfa/SOmedical/somedical/SomedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfa.SOmedical.somedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
