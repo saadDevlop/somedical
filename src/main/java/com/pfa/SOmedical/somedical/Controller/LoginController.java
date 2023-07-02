8d9d03a3314355939de085e78daa6b3a37a41ddf
saadDevlop <105066118+saadDevlop@users.noreply.github.com>
2023-07-02 15:58:01 +0100

    add api login

--- src/main/java/com/pfa/SOmedical/somedical/Controller/LoginController.java
+++ src/main/java/com/pfa/SOmedical/somedical/Controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.pfa.SOmedical.somedical.Controller;
-
-
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,26 +8,58 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.pfa.SOmedical.somedical.DAO.MedecinRepository;
 import com.pfa.SOmedical.somedical.Entities.Medecin;
-
+import com.pfa.SOmedical.somedical.Entities.Patient;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@Controller
+
+@RestController
+@RequestMapping("/api")
 public class LoginController {
-	@Autowired
 	
 	
-	@GetMapping("/login")
-	public String login() {
-		return"login";
+	
+
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public ResponseEntity<String> login(@RequestBody Patient loginRequest){
+		   if (isValidCredentials(loginRequest.getMail(),loginRequest.getMDP())) {
+	            String token = generateToken(loginRequest.getNomPatient());
+	            return ResponseEntity.ok("success");
+
+		   }else {
+			   return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		   }
+		   
+		
 	}
+
+  
+  private String generateToken(String patname) {
+      // Generate and return a token for the authenticated user
+      // You can use libraries like JWT to generate a token
+      // For simplicity, let's return a plain string token
+	  return "your_token_here";
+  }
 	
 	
 	
+	private boolean isValidCredentials(String mail, String password) {
+     
+        return mail.equals("admin") && password.equals("password");
+    }
+	
+
+
+
 	
 	
 	
