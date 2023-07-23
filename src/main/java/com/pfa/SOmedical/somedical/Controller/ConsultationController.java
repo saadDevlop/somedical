c1dbebfd560e9a415f7a7758d60646feaed311a0
Oumaima ELHANKARI <elhankari.oumaima@gmail.com>
2023-07-23 11:43:51 +0100

    dernier commit

--- src/main/java/com/pfa/SOmedical/somedical/Controller/ConsultationController.java
+++ src/main/java/com/pfa/SOmedical/somedical/Controller/ConsultationController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import com.pfa.SOmedical.somedical.DAO.ConsultationRepository;
 import com.pfa.SOmedical.somedical.Entities.Consultation;
 
